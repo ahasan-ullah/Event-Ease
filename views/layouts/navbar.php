a423642efc81eca122a2b4d4acfdcf68cab76b47
ahasan-ullah <ahasanullah451@gmail.com>
2025-06-16 01:11:38 +0600

    profile button added to the navbar and after succesfully login the profile and logout button will show dynamically

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -1,3 +1,8 @@
+<?php
+if(session_start()===PHP_SESSION_NONE){
+  session_start();
+}
+?>
 <nav class="navbar">
   <div class="logo">Event<br>Ease</div>
   <div class="nav-links">
@@ -10,7 +15,14 @@
     </ul>
   </div>
   <div class="btn-container">
-    <a href="../../../event-ease/views/login_page.php" class="login-btn" id="login-btn">Login</a>
+    <?php
+    if(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+      echo '<button class="profile-btn"  id="profile-btn"><a href="../../../event-ease/views/profile_page.php"><i class="fa fa-user"></i></a></button>';
+      echo '<a href="../../../event-ease/controllers/logout.php" class="login-btn">Logout</a>';
+    } else {
+      echo '<a href="../../../event-ease/views/login_page.php" class="login-btn" id="login-btn">Login</a>';
+    }
+    ?>
     <!-- mobile menu button-->
     <div class="mobile-menu-btn" id="mobile-menu-btn">
       <i class="fas fa-bars"></i>
