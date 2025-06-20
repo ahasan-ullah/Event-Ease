3544fbf38161711437ecd6b59201d9a586b351e2
ahasan-ullah <ahasanullah451@gmail.com>
2025-06-20 21:01:07 +0600

    login model and controller separated

--- models/auth_model.php
+++ models/auth_model.php
@@ -1,6 +1,39 @@
 <?php
 include '../config/db.php';
 $_SESSION['success'] = "";
+$_SESSION['login_failed'] = '';
+$_SESSION['user'] = [];
+
+function Login($email,$password)
+{
+  global $conn;
+  $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+    $_SESSION['user'] = [
+      'id' => $user['id'],
+      'name' => $user['name'],
+      'email' => $user['email'],
+      'user_type' => $user['user_type']
+    ];
+    // $_SESSION['login_failed'] = "Login successful! Welcome " . $_SESSION['user']['name'] . ".";
+    if ($_SESSION['user']['user_type'] === 'normal') {
+      $conn->close();
+      header("Location: ../../../../event-ease/index.php");
+      exit;
+    } elseif ($_SESSION['user']['user_type'] === 'admin') {
+      $conn->close();
+      header("Location: ../../../../event-ease/views/admin_dashboard.php");
+      exit;
+    }
+  } else {
+    $_SESSION['login_failed'] = "Invalid email or password.";
+  }
+  $conn->close();
+  header("Location: ../views/login_page.php");
+  exit;
+}
 
 function Register($name, $email, $password)
 {
