1b91953933cbad33b1d9e2fa1c267ccbbab9383b
ahasan-ullah <ahasanullah451@gmail.com>
2025-06-21 01:46:54 +0600

    add event form added but backend logic not added

---
+++ views/add_event.php
@@ -0,0 +1,55 @@
+<?php
+include '../config/db.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Add New Event</title>
+  <link rel="stylesheet" href="../public/styles/admin/add_event.css">
+  <script src="https://kit.fontawesome.com/8f7b27f9d3.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <?php
+  include '../views/layouts/admin_sidebar.php';
+  ?>
+  <main class="add_event_form">
+    <h2>Add New Event</h2>
+    <form action="store_event.php" method="POST" enctype="multipart/form-data">
+      <label>Title</label>
+      <input type="text" name="title" required />
+
+      <label>Price (BDT)</label>
+      <input type="number" name="price" step="50" required />
+
+      <label>Location</label>
+      <input type="text" name="location" required />
+
+      <label>Date</label>
+      <input type="date" name="date" required />
+
+      <label>Time</label>
+      <input type="text" name="time" placeholder="e.g., 6:30 PM" required />
+
+      <label>Category</label>
+      <input type="text" name="category" required />
+
+      <label>Organizer</label>
+      <input type="text" name="organizer" required />
+
+      <label>Available Seats</label>
+      <input type="number" name="available_seats" min="1" required />
+
+      <label>Image</label>
+      <input type="file" name="image" accept="image/*" required />
+
+      <label>Description</label>
+      <textarea name="description" rows="4" required></textarea>
+
+      <button type="submit">Add</button>
+    </form>
+  </main>
+</body>
+
+</html>
--- views/layouts/admin_sidebar.php
+++ views/layouts/admin_sidebar.php
@@ -18,7 +18,7 @@ $currentPath = $_SERVER['REQUEST_URI'];
       <li class="<?php echo strpos($currentPath, '/event-ease/views/admin_dashboard.php') !== false ? 'active' : '' ?>"><i class="fas fa-tachometer-alt"></i><a href="../views/admin_dashboard.php">Dashboard</a></li>
       <li class="<?php echo strpos($currentPath, '/event-ease/views/manage_users.php') !== false ? 'active' : '' ?>"><i class="fas fa-layer-group"></i><a href="../views/manage_users.php">Manage Users</a></li>
       <li><i class="fas fa-calendar-alt"></i><a href="#">Manage Events</a></li>
-      <li><i class="fas fa-users"></i><a href="#">Add Event</a></li>
+      <li class="<?php echo strpos($currentPath, '/event-ease/views/add_event.php') !== false ? 'active' : '' ?>"><i class="fas fa-users"></i><a href="../views/add_event.php">Add Event</a></li>
       <li><i class="fas fa-home"></i><a href="../index.php">Home</a></li>
       <li><i class="fas fa-cogs"></i><a href="../controllers/logout_controller.php">Logout</a></li>
     </ul>
