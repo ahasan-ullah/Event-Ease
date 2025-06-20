8b2cd950dfbf0ff9622488022d7145ea34d2879d
ahasan-ullah <ahasanullah451@gmail.com>
2025-06-20 23:53:24 +0600

    manage users static table added

--- views/manage_users.php
+++ views/manage_users.php
@@ -2,12 +2,27 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Manage Users</title>
+  <link rel="stylesheet" href="../public/styles/admin/manage_users.css">
 </head>
 <body>
   <?php
   include __DIR__ . "/layouts/admin_sidebar.php";
   ?>
+  <main class="user-table">
+    <h1>Manage Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>User Type</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        
+      </table>
+  </main>
 </body>
 </html>
