6260fbd644801df56522d6ae7479c2ed92d9f275
ahasan-ullah <ahasanullah451@gmail.com>
2025-06-20 21:55:43 +0600

    admin dashboard sidebar added

--- views/admin_dashboard.php
+++ views/admin_dashboard.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Dashboard</title>
+  <link rel="stylesheet" href="../public/styles/admin/admin_dashboard.css">
+  <script src="https://kit.fontawesome.com/8f7b27f9d3.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-  
+  <?php
+  include __DIR__ . "/layouts/admin/admin_sidebar.php";
+  ?>
 </body>
+
 </html>
---
+++ views/layouts/admin/admin_sidebar.php
@@ -0,0 +1,33 @@
+<?php
+$currentPath=$_SERVER['REQUEST_URI'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Admin Dashboard | EventEase</title>
+  <link rel="stylesheet" href="../public/styles/admin/admin_sidebar.css" />
+  <script src="https://kit.fontawesome.com/8f7b27f9d3.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <!-- <?php echo $currentPath ?> -->
+  <div class="sidebar-container">
+    <!-- sidbar -->
+    
+    <div class="sidebar">
+      <h2 class="sidebar-title">EventEase Admin</h2>
+      <ul class="sidebar-menu">
+        <li class="<?php echo strpos($currentPath,'/event-ease/views/admin_dashboard.php')!==false?'active':'' ?>"><i class="fas fa-tachometer-alt"></i><a href="#">Dashboard</a></li>
+        <li><i class="fas fa-layer-group"></i><a href="#">Manage Users</a></li>
+        <li><i class="fas fa-calendar-alt"></i><a href="#">Manage Events</a></li>
+        <li><i class="fas fa-users"></i><a href="#">Add Event</a></li>
+        <li><i class="fas fa-home"></i><a href="../index.php">Home</a></li>
+        <li><i class="fas fa-cogs"></i><a href="#">Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</body>
+
+</html>
